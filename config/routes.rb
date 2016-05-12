1d04633369555710508c6c7559e0fcc20f6d8182
Hafiz Aina <hafizaina94@gmail.com>
2016-05-11 23:12:39 -0400

    The user is able to view the list of books in the database.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  resources :books
   resources :posts
   resources :comments, only: [:create, :destroy]
   devise_for :users
