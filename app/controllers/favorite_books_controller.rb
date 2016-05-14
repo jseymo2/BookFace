9b3836b8448023ebca529457b7d39792ff05529b
Hafiz Aina <hafizaina94@gmail.com>
2016-05-14 00:09:23 -0400

    Users can successfully add books to their favorites list.

---
+++ app/controllers/favorite_books_controller.rb
@@ -0,0 +1,17 @@
+class FavoriteBooksController < ApplicationController
+
+  layout 'application'
+
+  def create
+
+    favorite_book = FavoriteBook.create(user_id: params[:user_id], book_id: params[:book_id])
+    if favorite_book
+      flash[:notice] = "#{params[:book_name]} has been successfully added to your favorites list."
+      redirect_to :back 
+    else
+      redirect_to books_path notice: favorite_book.errors.full_messages.first
+    end
+    
+  end
+
+end
